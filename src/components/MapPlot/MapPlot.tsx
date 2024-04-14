cf8223ee2c751361de957571583a33f894b01248
estebanix <dakisunshine@gmail.com>
2024-04-14 18:13:58 +0200

    Basic MapPlot

---
+++ src/components/MapPlot/MapPlot.tsx
@@ -0,0 +1,43 @@
+import * as d3 from "d3";
+import { FeatureCollection } from "geojson";
+
+import styles from "./MapPlot.module.scss";
+
+type MapProps = {
+  width: number;
+  height: number;
+  data: FeatureCollection;
+};
+
+export const MapPlot = ({ width, height, data }: MapProps) => {
+  const projection = d3
+    .geoMercator()
+    .scale(width)
+    .center([40, 40]);
+
+  const geoPathGenerator = d3.geoPath().projection(projection);
+
+  const allSvgPaths = data.features
+    .filter((shape) => shape.id !== "ATA")
+    .map((shape) => {
+      return (
+        <path
+          key={shape.id}
+          className={styles.shape}
+          d={geoPathGenerator(shape)}
+          stroke="black"
+          strokeWidth={0.5}
+          fill="#6f777d"
+          fillOpacity={0.7}
+        />
+      );
+    });
+
+  return (
+    <div className={styles.mapPlotContainer}>
+      <svg width={width} height={height}>
+        {allSvgPaths}
+      </svg>
+    </div>
+  );
+};
--- src/containers/HomePage/HomePage.tsx
+++ src/containers/HomePage/HomePage.tsx
@@ -1,4 +1,7 @@
 import { HeaderMenu } from "../../components/HeaderMenu";
+import { MapPlot } from "../../components/MapPlot";
+
+import {data} from "./data";
 
 import styles from "./HomePage.module.scss";
 
@@ -6,6 +9,7 @@ export const HomePage = () => {
   return (
     <div className={styles.homePageContainer}>
       <HeaderMenu />
+      <MapPlot data={data} width={450} height={90} />
     </div>
   );
 };
