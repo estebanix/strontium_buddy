8bf2224ece30afc84d8c9ee8bfd7455ba3a355cc
estebanix <dakisunshine@gmail.com>
2024-04-16 15:56:35 +0200

    render fix

--- src/components/PlotContainer/PlotContainer.tsx
+++ src/components/PlotContainer/PlotContainer.tsx
@@ -1,13 +1,17 @@
 import { DropDown } from "../DropDown/DropDown";
 import { BarChart } from "../Plots/BarChart";
+import { useContext } from "react";
+import { Context } from "../../context/Context";
 
 import styles from "./PlotContainer.module.scss";
 
 export const PlotContainer = () => {
-    return(
+    const { currentPlot } = useContext(Context);
+
+    return (
         <div className={styles.plotContainerContainer}>
             <DropDown />
-            <BarChart width={500} height={350} />
+            {currentPlot && <BarChart width={500} height={350} />}
         </div>
-    )
-}
+    );
+};
