8258e18933e05bc04dc4537728e932dbbc55116d
estebanix <dakisunshine@gmail.com>
2024-04-16 14:49:49 +0200

    Footer components

--- src/components/DropDown/DropDown.tsx
+++ src/components/DropDown/DropDown.tsx
@@ -2,17 +2,11 @@ import Select from "react-select";
 import { useContext } from "react";
 import { Context } from "../../context/Context";
 import { OptionsProps } from "../../utils/types/types";
+import { options } from "../../utils/constans/plotOptions";
 
 import styles from "./DropDown.module.scss";
 
 export const DropDown = () => {
-  const options = [
-    { value: "line", label: "Line Plot" },
-    { value: "bar", label: "Bar Chart" },
-    { value: "scatter", label: "Scatter Plot" },
-    { value: "histogram", label: "Histogram" },
-  ];
-
   const { currentPlot, setCurrentPlot } = useContext(Context);
 
   const handleSelection = (selectedOption: OptionsProps | null) => {
---
+++ src/utils/constans/plotOptions.ts
@@ -0,0 +1,6 @@
+export const options = [
+    { value: "line", label: "Line Plot" },
+    { value: "bar", label: "Bar Chart" },
+    { value: "scatter", label: "Scatter Plot" },
+    { value: "histogram", label: "Histogram" },
+  ];
