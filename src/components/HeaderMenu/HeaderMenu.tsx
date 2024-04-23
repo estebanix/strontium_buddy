ef3e3cada7b997d5fed627aa26bdd01d6106e10d
estebanix <dakisunshine@gmail.com>
2024-04-23 10:20:42 +0200

    Small HeaderMenu redesign

--- src/components/HeaderMenu/HeaderMenu.tsx
+++ src/components/HeaderMenu/HeaderMenu.tsx
@@ -1,12 +1,13 @@
 import { headerMenuItems } from "../../utils/constans/headerMenuItems";
 import { Link } from "react-router-dom";
+import { BoxRow } from "../BoxRow";
 
 import styles from "./HeaderMenu.module.scss";
 
 export const HeaderMenu = () => {
   return (
     <div className={styles.headerMenuContainer}>
-      <div className={styles.headerMenuItemBox}>
+      <BoxRow gapWidth={30}>
         {headerMenuItems.map((item, index) => {
           return (
             <Link key={index} to={item.to} className={styles.headerMenuLink}>
@@ -14,7 +15,7 @@ export const HeaderMenu = () => {
             </Link>
           );
         })}
-      </div>
+      </BoxRow>
     </div>
   );
 };
