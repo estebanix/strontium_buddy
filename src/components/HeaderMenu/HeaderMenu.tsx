03d83cc5715f5f3515a1861ffd176fd13c8fd82a
estebanix <dakisunshine@gmail.com>
2024-04-23 11:09:24 +0200

    Redesigning Button and HeaderMenu

--- src/components/HeaderMenu/HeaderMenu.tsx
+++ src/components/HeaderMenu/HeaderMenu.tsx
@@ -1,21 +1,29 @@
 import { headerMenuItems } from "../../utils/constans/headerMenuItems";
 import { Link } from "react-router-dom";
 import { BoxRow } from "../BoxRow";
+import { Image } from "../Image";
+import headerLogo from "./../../../public/images/React-icon.svg.png";
 
 import styles from "./HeaderMenu.module.scss";
 
 export const HeaderMenu = () => {
   return (
     <div className={styles.headerMenuContainer}>
-      <BoxRow gapWidth={30}>
-        {headerMenuItems.map((item, index) => {
-          return (
-            <Link key={index} to={item.to} className={styles.headerMenuLink}>
-              <p>{item.title}</p>
-            </Link>
-          );
-        })}
+      <BoxRow gapWidth={15}>
+        <Image src={headerLogo} />
+        <h4>SrBuddy</h4>
       </BoxRow>
+      <div className={styles.headerOptionsBox}>
+        <BoxRow gapWidth={30}>
+          {headerMenuItems.map((item, index) => {
+            return (
+              <Link key={index} to={item.to} className={styles.headerMenuLink}>
+                <p>{item.title}</p>
+              </Link>
+            );
+          })}
+        </BoxRow>
+      </div>
     </div>
   );
 };
